f5d414fb0aeb452666426d251a5e95317f8ecb84
git <lipusipupara@gmail.com>
2015-07-17 22:27:53 +0530

    index statement is done

--- Day1.sql
+++ Day1.sql
@@ -27,16 +27,39 @@ set @kk=(select name ,case when gender="M" then  "F"
                   else "not defined"
              end as sex from tb);
 
-select @kk;
 
 
-update tb inner join (select id, name ,case when gender="M" then  "F" 
-											when gender="F" then "M"
-											else "not defined"
-                                            end as sex from tb) as kk on  kk.id=tb.id
- set tb.gender=kk.sex;
+select name into @ss from tb where id=2; 
+select @ss;
 
-select * from tb;
+
+
+select "commint" into @so;
+
+select  @para;
+
+
+
+
+
+
+insert into tb1(name,gender) value ("lipu",1),("lipu2",2),("lipu3",2),("lipu4",1),("lipu5",2);
+
+insert into tb2 (idtb2,gen) values (1,"male"),(2,"female");
+
+insert into tb2 (idtb2,gen) values (3,"transgender");
+
+select * from tb2;
+
+
+delete from tb2 where gen="male";
+
+create  index ind4 on tb1 (gender);
+
+show index from tb1;
+
+drop index ind4 on tb1;
+
 
 
 
