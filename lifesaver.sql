79e3fb90a5b428a6cbdfcfca8875bdbd2ea5e9a2
git <lipusipupara@gmail.com>
2016-03-21 23:25:08 +0530

    getting random value

--- lifesaver.sql
+++ lifesaver.sql
@@ -1,10 +1,17 @@
 -- how to export data to mysql file from database
 mysqldump -u username -p password > mysqlfile.sql
 
--- to make it compress 
+-- to make it compress
 
 tar zcf dump.tar.gz mysqlfile.sql
 
 -- to extract
 
 tar zxf dump.tar.gz
+
+
+-- how to insert random value into table
+
+floor(rand()*91)+10
+
+floor(rand()*max-min+1)+min
